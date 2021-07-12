18bf8514e3446045e2a5506dbf01bdf14412ccf0
Shreya Verma <72500761+shreya-v7@users.noreply.github.com>
2021-07-12 23:27:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import postRoutes from './routes/posts.js'
+
+const app = express();
+
+app.use('/posts', postRoutes);
+
+app.use(bodyParser.json({ limit: "50mb", extended: true}));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true}));
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://shreya_blog:Sverma@$7@blog.ptv20.mongodb.net/test';
+const PORT = process.env.PORT || 8080;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
